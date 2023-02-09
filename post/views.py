f994f30bc6d12b42a04c0c19a31541f3dfe3639f
sanjay <sanjaypython22@gmail.com>
2023-02-09 22:59:03 +0530

    index design

--- post/views.py
+++ post/views.py
@@ -8,21 +8,24 @@ from category.models import CategoryMaster
 
 def home_page(request):
     ads = Advertisement.objects.get(ads_name='TOP1')
-    ads1 = Advertisement.objects.get(ads_name='TOP2')
     top1 = ads.advert.url
+    ads1 = Advertisement.objects.get(ads_name='TOP2')
     top2 = ads1.advert.url
-    post = PostMaster.objects.all()
+    side2 = Advertisement.objects.get(ads_name='SIDE2')
+    side2 = side2.advert.url
+    post = PostMaster.objects.all().order_by('-created_at')[:25]
     category = CategoryMaster.objects.all()
     context = {
         'top1':top1,
         'top2':top2,
+        'side2':side2,
         'post':post,
         'category':category,
     }
     return render(request, 'index.html',context)
 
 def get_data_by_cat(request,id):
-    data = PostMaster.objects.filter(post_category_id=id)
+    data = PostMaster.objects.filter(post_category_id=id).order_by('-created_at')
     if data:
         data_list = []
         for i in data:
@@ -43,9 +46,11 @@ def get_data_by_cat(request,id):
 
 def view_post_details(request,obj_id):
     obj_id = obj_id
+    post = PostMaster.objects.all().order_by('-created_at')[:5]
     single_post = PostMaster.objects.get(id=obj_id)
     context = {
         'obj_id':obj_id,
+        'post':post,
         'post_data':single_post,
     }
     return render(request,'post_detail.html',context)
