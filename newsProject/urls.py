2117709224949c07c9ab799a4dc3254df3bcd30c
sanjay1994singh <121377604+sanjay1994singh@users.noreply.github.com>
2023-02-11 17:42:03 +0530

    Update urls.py

--- newsProject/urls.py
+++ newsProject/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('source admin/', admin.site.urls),
     path('',include('post.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
