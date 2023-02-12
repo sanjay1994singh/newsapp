dad98122cf79764f51d29781ea32242d5b74e340
sanjay1994singh <121377604+sanjay1994singh@users.noreply.github.com>
2023-02-12 12:39:15 +0530

    Update settings.py

--- newsProject/settings.py
+++ newsProject/settings.py
@@ -132,6 +132,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'theme', 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
