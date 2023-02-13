418695411cfd210f104a65293696d3f24d343291
sanjay <sanjaypython22@gmail.com>
2023-02-13 20:57:25 +0530

    settings debug tru

--- newsProject/urls.py
+++ newsProject/urls.py
@@ -24,7 +24,7 @@ from django.views.static import serve
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    path('source admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',include('post.urls')),
 ]#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
